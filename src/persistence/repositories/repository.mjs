db5b379f39e196b5533a180bdf86a2b19670a40c
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-12-06 18:35:43 +0100

    Added transaction support to repository.

--- src/persistence/repositories/repository.mjs
+++ src/persistence/repositories/repository.mjs
@@ -4,7 +4,30 @@ import { Cloneable } from '../../misc/stampit-util.mjs'
 
 export const Repository = stampit(DatabaseUtil, Cloneable, {
   name: 'Repository',
+  statics: {
+    // Calls `callback` with transactional versions of `repositories`. All repository actions
+    // performed inside `callback` will belong to the same transaction.
+    async withTransaction (repositories, callback) {
+      if (repositories.length === 0) {
+        return await callback([])
+      }
+
+      const knex = repositories[0].knex
+
+      return await knex.transaction(async transactionalKnex => {
+        const transactionalRepositories = repositories.map(repository => {
+          const clone = repository.clone()
+          clone.knex = transactionalKnex
+          return clone
+        })
+
+        return await callback(transactionalRepositories)
+      })
+    }
+  },
   methods: {
+    // Calls `callback` with transactional version of this repository. All repository actions
+    // performed inside `callback` will belong to the same transaction.
     async withTransaction (callback) {
       return await this.knex.transaction(async knex => {
         const clone = this.clone()
