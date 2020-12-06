a61d8bf0a2d381674e2d8ade66e37acb396930c6
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-12-06 14:12:24 +0100

    Refactoring with stampit.

--- src/ioc-container.mjs
+++ src/ioc-container.mjs
@@ -10,151 +10,157 @@ import { I18nService } from './i18n/i18n-service.mjs'
 import { DataInitializer } from './persistence/database/data-initializer.mjs'
 import { WebInitializer } from './web/web-initializer.mjs'
 import { knexFactory } from './persistence/database/knex.mjs'
+import stampit from '@stamp/it'
 
 // Resolves dependencies for each 'component' in the application.
-export const IocContainer = ({
-  environment,
-  knex,
-  webInitializer,
-  dataInitializer,
-  databaseManager,
-  usersRepository,
-  cryptographyService,
-  emailService,
-  authenticationService,
-  usersService,
-  authenticationRouter,
-  usersRouter,
-  i18nService
-}) => {
-  const getKnex = () => {
-    if (knex === undefined) {
-      knex = knexFactory(environment)
+export const IocContainer = stampit({
+  init ({
+    environment,
+    knex,
+    webInitializer,
+    dataInitializer,
+    databaseManager,
+    usersRepository,
+    cryptographyService,
+    emailService,
+    authenticationService,
+    usersService,
+    authenticationRouter,
+    usersRouter,
+    i18nService
+  }) {
+    this.environment = environment
+    this.knex = knex
+    this.webInitializer = webInitializer
+    this.dataInitializer = dataInitializer
+    this.databaseManager = databaseManager
+    this.usersRepository = usersRepository
+    this.cryptographyService = cryptographyService
+    this.emailService = emailService
+    this.authenticationService = authenticationService
+    this.usersService = usersService
+    this.authenticationRouter = authenticationRouter
+    this.usersRouter = usersRouter
+    this.i18nService = i18nService
+  },
+  methods: {
+    getEnvironment () {
+      return this.environment
+    },
+
+    getKnex () {
+      if (this.knex === undefined) {
+        this.knex = knexFactory(this.getEnvironment())
+      }
+      return this.knex
+    },
+
+    getDatabaseManager () {
+      if (this.databaseManager === undefined) {
+        this.databaseManager = DatabaseManager({
+          knex: this.getKnex(),
+          environment: this.getEnvironment()
+        })
+      }
+      return this.databaseManager
+    },
+
+    getDataInitializer () {
+      if (this.dataInitializer === undefined) {
+        this.dataInitializer = DataInitializer({
+          knex: this.getKnex(),
+          environment: this.getEnvironment(),
+          cryptographyService: this.getCryptographyService(),
+        })
+      }
+      return this.dataInitializer
+    },
+
+    getUsersRepository () {
+      if (this.usersRepository === undefined) {
+        this.usersRepository = UsersRepository({
+          knex: this.getKnex(),
+          environment: this.getEnvironment()
+        })
+      }
+      return this.usersRepository
+    },
+
+    getI18nService () {
+      if (this.i18nService === undefined) {
+        this.i18nService = I18nService({
+          environment: this.getEnvironment()
+        })
+      }
+      return this.i18nService
+    },
+
+    getCryptographyService () {
+      if (this.cryptographyService === undefined) {
+        this.cryptographyService = CryptographyService({
+          environment: this.getEnvironment()
+        })
+      }
+      return this.cryptographyService
+    },
+
+    getEmailService () {
+      if (this.emailService === undefined) {
+        this.emailService = EmailService({
+          i18nService: this.getI18nService(),
+          environment: this.getEnvironment()
+        })
+      }
+      return this.emailService
+    },
+
+    getAuthenticationService () {
+      if (this.authenticationService === undefined) {
+        this.authenticationService = AuthenticationService({
+          environment: this.getEnvironment(),
+          usersRepository: this.getUsersRepository(),
+          cryptographyService: this.getCryptographyService(),
+          emailService: this.getEmailService()
+        })
+      }
+      return this.authenticationService
+    },
+
+    getUsersService () {
+      if (this.usersService === undefined) {
+        this.usersService = UsersService({
+          usersRepository: this.getUsersRepository()
+        })
+      }
+      return this.usersService
+    },
+
+    getAuthenticationRouter () {
+      if (this.authenticationRouter === undefined) {
+        this.authenticationRouter = AuthenticationRouter({
+          authenticationService: this.getAuthenticationService()
+        })
+      }
+      return this.authenticationRouter
+    },
+
+    getUsersRouter () {
+      if (this.usersRouter === undefined) {
+        this.usersRouter = UsersRouter({
+          usersService: this.getUsersService()
+        })
+      }
+      return this.usersRouter
+    },
+
+    getWebInitializer () {
+      if (this.webInitializer === undefined) {
+        this.webInitializer = WebInitializer({
+          authenticationRouter: this.getAuthenticationRouter(),
+          usersRouter: this.getUsersRouter()
+        })
+      }
+      return this.webInitializer
     }
-    return knex
   }
-
-  const getDatabaseManager = () => {
-    if (databaseManager === undefined) {
-      databaseManager = DatabaseManager({
-        knex: getKnex(),
-        environment
-      })
-    }
-    return databaseManager
-  }
-
-  const getDataInitializer = () => {
-    if (dataInitializer === undefined) {
-      dataInitializer = DataInitializer({
-        knex: getKnex(),
-        environment,
-        cryptographyService: getCryptographyService(),
-      })
-    }
-    return dataInitializer
-  }
-
-  const getUsersRepository = () => {
-    if (usersRepository === undefined) {
-      usersRepository = UsersRepository({
-        knex: getKnex(),
-        environment
-      })
-    }
-    return usersRepository
-  }
-
-  const getI18nService = () => {
-    if (i18nService === undefined) {
-      i18nService = I18nService({ environment })
-    }
-    return i18nService
-  }
-
-  const getCryptographyService = () => {
-    if (cryptographyService === undefined) {
-      cryptographyService = CryptographyService({ environment })
-    }
-    return cryptographyService
-  }
-
-  const getEmailService = () => {
-    if (emailService === undefined) {
-      emailService = EmailService({
-        i18nService: getI18nService(),
-        environment
-      })
-    }
-    return emailService
-  }
-
-  const getAuthenticationService = () => {
-    if (authenticationService === undefined) {
-      authenticationService = AuthenticationService({
-        environment,
-        usersRepository: getUsersRepository(),
-        cryptographyService: getCryptographyService(),
-        emailService
-      })
-    }
-    return authenticationService
-  }
-
-  const getUsersService = () => {
-    if (usersService === undefined) {
-      usersService = UsersService({
-        usersRepository: getUsersRepository()
-      })
-    }
-    return usersService
-  }
-
-  const getAuthenticationRouter = () => {
-    if (authenticationRouter === undefined) {
-      authenticationRouter = AuthenticationRouter({
-        authenticationService: getAuthenticationService()
-      })
-    }
-    return authenticationRouter
-  }
-
-  const getUsersRouter = () => {
-    if (usersRouter === undefined) {
-      usersRouter = UsersRouter({
-        usersService: getUsersService()
-      })
-    }
-    return usersRouter
-  }
-
-  const getWebInitializer = () => {
-    if (webInitializer === undefined) {
-      webInitializer = WebInitializer({
-        authenticationRouter: getAuthenticationRouter(),
-        usersRouter: getUsersRouter()
-      })
-    }
-    return webInitializer
-  }
-
-  return ({
-    getKnex,
-
-    getDatabaseManager,
-    getDataInitializer,
-
-    getUsersRepository,
-
-    getI18nService,
-    getEmailService,
-    getCryptographyService,
-
-    getAuthenticationService,
-    getUsersService,
-
-    getWebInitializer,
-    getAuthenticationRouter,
-    getUsersRouter
-  })
-}
+})
